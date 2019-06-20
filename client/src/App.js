b7cac1da0c03513691f6220d671d25c19219cb63
gizemocak <gizem_ocak@outlook.com>
2019-06-20 14:04:28 -0400

    connect clients side and server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import List from './components/List';
