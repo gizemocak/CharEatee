a9d4436b1ce818e596728d3cc457041a37600419
gizemocak <gizem_ocak@outlook.com>
2019-06-20 13:35:28 -0400

    add home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/list' component={List}/>
+    </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default class Home extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h1>Project Home</h1>
+      {/* Link to List.js */}
+      <Link to={'./list'}>
+        <button variant="raised">
+            My List
+        </button>
+      </Link>
+    </div>
+    );
+  }
+}
