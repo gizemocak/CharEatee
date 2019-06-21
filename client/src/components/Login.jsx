464682f742e10f3102639ae2ec46ec1901066227
gizemocak <gizem_ocak@outlook.com>
2019-06-21 16:02:15 -0400

    create login form

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import NavBar from './NavBar'
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <>
+      <h1>Login</h1>
+      <NavBar/>
+      <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+    
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicChecbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </>
+    );
+  }
+}
