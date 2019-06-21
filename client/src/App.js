3676e76796e6082713d774c0a3ccb86a934eb5d3
gizemocak <gizem_ocak@outlook.com>
2019-06-21 15:55:23 -0400

    add routes for the components

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,23 @@ import { Route, Switch, withRouter} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import List from './components/List';
+import SignUpForm from './components/SignupForm';
+import Login from './components/Login';
+import CharityHome from './components/CharityHome';
+import GroceryHome from './components/GroceryHome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/list' component={List}/>
-    </Switch>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/list' component={List}/>
+          <Route path='/signup' component={SignUpForm}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/grocery/home/:id' component={GroceryHome}/>
+          <Route path='/charity/home/:id' component={CharityHome}/>
+        </Switch>
       </div>
     );
   }
