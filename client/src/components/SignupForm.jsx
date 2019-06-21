6c051c253022343f9525574c3edade86a78738a2
gizemocak <gizem_ocak@outlook.com>
2019-06-21 16:05:43 -0400

    create signup form component

---
+++ client/src/components/SignupForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+
+
+export default class SignUpForm extends Component {
+  render() {
+    return (
+    <div>I am the sign up form</div>
+    );
+  }
+}
