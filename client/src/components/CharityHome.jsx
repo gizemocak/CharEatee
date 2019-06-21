519df771034e157ee60a16060cf22cd0bad7057e
gizemocak <gizem_ocak@outlook.com>
2019-06-21 16:00:19 -0400

    create charity home component and add navbar

---
+++ client/src/components/CharityHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+
+export default class CharityHome extends Component {
+  render() {
+    return (
+      <>
+        <NavBar />
+        <div>I am Charity Home</div>
+      </>
+    );
+  }
+}
