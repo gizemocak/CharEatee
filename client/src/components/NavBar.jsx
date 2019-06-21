393c3b8fbda3b415985b7e03babc65e5d35e36bb
gizemocak <gizem_ocak@outlook.com>
2019-06-21 16:04:50 -0400

    create navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Navbar bg="light" variant="light" fixed="top">
+        <Navbar.Brand href="/">
+          <img src="/images/watermelon.png" width="30" height="30" />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="#profile">Profile</Nav.Link>
+        </Nav>
+        {this.props.camera && (
+          <Navbar.Brand href="/">
+            <img src="/images/camera.png" width="30" height="30" />
+          </Navbar.Brand>
+        )}
+      </Navbar>
+    );
+  }
+}
