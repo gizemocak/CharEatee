f11f231810bf1813890f1c1a9127e8523c38b7f7
gizemocak <gizem_ocak@outlook.com>
2019-06-23 17:53:58 -0400

    pin user's current location on the map

--- client/src/components/CharityHome.jsx
+++ client/src/components/CharityHome.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import MapContainer from "./MapContainer";
 import NavBar from "./NavBar";
 
-function CharityHome() {
+export default function CharityHome() {
   const [apiKey, setApiKey] = useState(null);
+  const [geoLoc, setGeoLoc] = useState({});
 
   useEffect(() => {
+    getGeoLocation();
     fetch("/api/getApiKey")
       .then(res => res.json())
       .then(data => setApiKey(data.apiKey))
@@ -14,13 +16,23 @@ function CharityHome() {
       });
   }, []);
 
+  const getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        setGeoLoc(pos);
+      });
+    }
+  };
+
   return (
     <>
       <div>Charity Home page</div>
       <NavBar />
-      {apiKey && <MapContainer apiKey={apiKey} />}
+      {apiKey && <MapContainer apiKey={apiKey} geoLocation={geoLoc} />}
     </>
   );
 }
-
-export default CharityHome;
--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -4,7 +4,11 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 class GoogleMap extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        initialCenter={this.props.geoLocation}
+      >
         <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
         <InfoWindow onClose={this.onInfoWindowClose} />
