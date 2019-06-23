970f53e7be714f07c4d1248e77abaa6050aed54d
gizemocak <gizem_ocak@outlook.com>
2019-06-23 18:26:22 -0400

    add sme charity locations to the map

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -11,6 +11,16 @@ class GoogleMap extends Component {
       >
         <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
+        <Marker
+          title={"Charity"}
+          name={"Canadian Foundation for Health and Prosperity"}
+          position={{ lat: 43.663788, lng: -79.3782 }}
+        />
+        <Marker
+          name={"CanadaHelps"}
+          position={{ lat: 43.65325, lng: -79.3983 }}
+        />
+
         <InfoWindow onClose={this.onInfoWindowClose} />
       </Map>
     );
