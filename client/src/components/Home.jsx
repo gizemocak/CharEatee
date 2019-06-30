764086f2efcfc016b54b92bb9bccfe241655f805
Chenchen Kuang <kcchaha@gmail.com>
2019-06-30 18:40:50 -0400

    create animation for loading HomeSub page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "./NavBar";
 import { Link } from "react-router-dom";
-import "../Style/Home.scss"
+import "../style/Home.scss";
+
+
+import styled, { keyframes } from "styled-components";
+import {flipInY} from 'react-animations';
+const FlipInYAnimation = keyframes`${flipInY}`;
+const FlipInYDiv = styled.div`
+  animation: infinite 5s ${FlipInYAnimation};
+`;
 
 export default class Home extends Component {
   render() {
+    const { isOpen, onClose, message } = this.props;
     return (
       <div className="App">
         <Navbar />
-        <div className="body">
-        <Link to={"/login"}>Grocery Store</Link>
-        <hr/>
-        <Link to={"/login"}>Charity</Link>
-        <hr/>
-        <Link to={"/signup"}>Don't have an account?Sign Up</Link>
-        <hr/>
-        <Link to={'/grocery/home/:id'}>Grocery Home</Link>
-        {/* Link to List.js */}
-        {/* <Link to={'./list'}>
-        <button variant="raised">
-            My List
-        </button>
-      </Link> */}
-      <hr/>
-        <Link to={"/charity/home/:id"}>Charity Home Page</Link>
-        </div>
+      
       </div>
     );
   }
