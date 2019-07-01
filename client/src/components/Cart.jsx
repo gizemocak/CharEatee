e867b22a126d1b98eead72cfa4efe953dc8a3a00
gizemocak <gizem_ocak@outlook.com>
2019-07-01 19:37:43 -0400

    add columns to orders table

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -12,13 +12,17 @@ export default function Cart(props) {
       props.history.push("/login");
     }
   }, []);
-
+  console.log("cart", cart);
+  console.log("user", JSON.parse(localStorage.getItem("user")).user_id);
   console.log(props);
   const placeOrder = () => {
+    let charityId = JSON.parse(localStorage.getItem("user")).user_id;
+    let charityObj = { charityId: charityId };
+    let info = [...cart, charityObj];
     fetch("http://localhost:8080/api/order", {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(cart)
+      body: JSON.stringify(info)
     }).then(res => {
       console.log("response", res);
     });
@@ -30,7 +34,12 @@ export default function Cart(props) {
       <div>Cart</div>
       {cart.length > 0 &&
         cart.map(item => {
-          return <li>{item.name} {item.quantity}{item.unit}</li>;
+          return (
+            <li>
+              {item.name} {item.quantity}
+              {item.unit}
+            </li>
+          );
         })}
 
       <Button onClick={placeOrder}>Place Order</Button>
