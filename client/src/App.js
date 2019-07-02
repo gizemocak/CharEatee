89053ae66fbe27cf7ae95e4f51dd01f7cda74105
gizemocak <gizem_ocak@outlook.com>
2019-07-02 11:15:08 -0400

    create a new store to kep order info

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,5 @@
-import React, {
-  Component,
-  useEffect
-} from 'react';
-import {
-  Route,
-  Switch,
-  withRouter
-} from 'react-router-dom';
+import React, { useEffect  } from 'react';
+import { Route, Switch, withRouter} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import List from './components/List';
@@ -15,10 +8,7 @@ import Login from './components/Login';
 import CharityHome from './components/CharityHome';
 import GroceryHome from './components/GroceryHome'
 import Profile from './components/Profile'
-import {
-  createStore,
-  StoreProvider
-} from 'easy-peasy'
+import {createStore, StoreProvider} from 'easy-peasy'
 import usersProductsModel from "./stores/usersProductsModel"
 import adminModel from "./stores/adminModel"
 import Cart from "./components/Cart"
@@ -35,72 +25,27 @@ const store = createStore({
 console.log(store)
 
 function App() {
-  useEffect(() => {
-    store.dispatch.fetchGoogleMapsAPIKey()
-  }, [])
-  return ( <
-    StoreProvider store = {
-      store
-    } >
-    <
-    div className = "App" >
-    <
-    Switch >
-    <
-    Route exact path = '/'
-    component = {
-      Home
-    }
-    /> <
-    Route path = '/list'
-    component = {
-      List
-    }
-    /> <
-    Route path = '/signup'
-    component = {
-      SignUpForm
-    }
-    /> <
-    Route path = '/login'
-    component = {
-      Login
-    }
-    /> <
-    Route path = '/grocery/home/:id'
-    component = {
-      GroceryHome
-    }
-    /> <
-    Route path = '/charity/home/:id'
-    component = {
-      CharityHome
-    }
-    /> <
-    Route path = '/profile/:id'
-    component = {
-      Profile
-    }
-    /> <
-    Route path = '/cart'
-    component = {
-      Cart
-    }
-    /> <
-    Route path = '/account'
-    component = {
-      HomeSub
-    }
-    /> <
-    Route path = '/order-history'
-    component = {
-      OrderHistory
-    }
-    /> <
-    /Switch> <
-    /div> <
-    /StoreProvider>
-  );
+    useEffect(() => {
+      store.dispatch.fetchGoogleMapsAPIKey()
+    },[])
+    return (
+      <StoreProvider store={store}>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/list' component={List}/>
+          <Route path='/signup' component={SignUpForm}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/grocery/home/:id' component={GroceryHome}/>
+          <Route path='/charity/home/:id' component={CharityHome}/>
+          <Route path='/profile/:id' component={Profile}/>
+          <Route path='/cart' component={Cart}/>
+          <Route path='/account' component={HomeSub}/>
+          <Route path='/order-history' component={OrderHistory}/>
+        </Switch>
+      </div>
+      </StoreProvider>
+    );
 }
 
 export default withRouter(App);
---
+++ client/src/stores/orderHistoryModel.js
@@ -0,0 +1,12 @@
+import {
+  action
+} from "easy-peasy";
+
+const order = {
+  order: [],
+  addToOrder: action((state, order) => {
+    state.order = [...state.order, order]
+  })
+};
+
+export default cart
