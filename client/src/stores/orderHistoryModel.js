83ec080df71ddb9d3c9a1db3b3eacdae4af34d33
gizemocak <gizem_ocak@outlook.com>
2019-07-02 11:46:37 -0400

    add action to order store

--- client/src/stores/orderHistoryModel.js
+++ client/src/stores/orderHistoryModel.js
@@ -6,7 +6,18 @@ const order = {
   order: [],
   addToOrder: action((state, order) => {
     state.order = [...state.order, order]
-  })
+  }),
+  fetchOrder: thunk(async actions => {
+    const res = await fetch('http://localhost:8080/api/orders', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    const orders = await res.json();
+
+    actions.addToOrder(orders);
+  }),
 };
 
 export default order
