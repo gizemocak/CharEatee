076aa3d4545391f9cf9365a1e324a00c1cc4ebb4
gizemocak <gizem_ocak@outlook.com>
2019-07-02 09:32:02 -0400

    resolve conflicts

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,12 +4,12 @@ import { useStoreState, useStoreActions, StoreProvider } from "easy-peasy";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import "../style/Profile.scss";
-import Card from 'react-bootstrap/Card';
-import Accordion from 'react-bootstrap/Accordion';
-import posed from 'react-pose';
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import posed from "react-pose";
 
 import styled, { keyframes } from "styled-components";
-import {fadeInUp} from 'react-animations';
+import { fadeInUp } from "react-animations";
 const FadeInUpAnimation = keyframes`${fadeInUp}`;
 const FadeInUpDiv = styled.div`
   animation: 2s ${FadeInUpAnimation};
@@ -20,15 +20,15 @@ const Hover = posed.div({
   pressable: true,
   init: {
     scale: 1,
-    boxShadow: '0px 0px 0px rgba(0,0,0,0)'
+    boxShadow: "0px 0px 0px rgba(0,0,0,0)"
   },
   hover: {
     scale: 1.2,
-    boxShadow: '0px 5px 10px rgba(0,0,0,0.2)'
+    boxShadow: "0px 5px 10px rgba(0,0,0,0.2)"
   },
   press: {
     scale: 1.1,
-    boxShadow: '0px 2px 5px rgba(0,0,0,0.1)'
+    boxShadow: "0px 2px 5px rgba(0,0,0,0.1)"
   }
 });
 
@@ -80,95 +80,124 @@ export default function Profile(props) {
     <div className="showItems">
       <NavBar />
       <FadeInUpDiv>
-      {user.type === "Charity" && <Link to="/cart">cart: {cart.length} </Link>}
+        {user.type === "Charity" && (
+          <Link to="/cart">cart: {cart.length} </Link>
+        )}
 
-      <h3> Welcome back, {username && username}</h3>
-      {user.type === "Grocer/Restaurant" && usersInfo &&
-      <div className="greeting">
-        <p>We really appreciate your kindness!</p>
-        <p>Click <Link to={`/grocery/home/${JSON.parse(localStorage.getItem('user')).user_id}`}>HERE</Link> if you want to make more donations</p>
-        <p>Or you can check your past donations below:</p>
-      </div>}
-
-      <ul>
-        {user.type === "Charity" && stores && (
-          <div>
-            {filteredStore.products &&
-              filteredStore.products.map(item => {
-                return (
-                  <div>
-                    <li>
-                      {item.name} {item.quantity} {item.unit}
-                      <Button
-                        onClick={() => {
-                          handleAddToCart(item);
-                        }}
-                        name="add to cart"
-                      />
-                    </li>
-                  </div>
-                );
-              })}
-            {cart.length > 0 && (
-              <Button
-                onClick={() => {
-                  props.history.push("/cart");
-                }}
+        <h3> Welcome back, {username && username}</h3>
+        {user.type === "Grocer/Restaurant" && stores && (
+          <div className="greeting">
+            <p>We really appreciate your kindness!</p>
+            <p>
+              Click{" "}
+              <Link
+                to={`/grocery/home/${
+                  JSON.parse(localStorage.getItem("user")).user_id
+                }`}
               >
-                Checkout {cart.length === 1 ? "1 Item" : `${cart.length} Items`}
-              </Button>
-            )}
+                HERE
+              </Link>{" "}
+              if you want to make more donations
+            </p>
+            <p>Or you can check your past donations below:</p>
           </div>
         )}
 
-        {user.type === "Grocer/Restaurant" &&
-          stores &&
-          stores.map(item => {
-            if (item.user_id === Number(props.match.params.id)) {
-              if (username !== item.username) {
-                setUserName(item.username);
+        <ul>
+          {user.type === "Charity" && stores && (
+            <div>
+              {filteredStore.products &&
+                filteredStore.products.map(item => {
+                  return (
+                    <div>
+                      <li>
+                        {item.name} {item.quantity} {item.unit}
+                        <Button
+                          onClick={() => {
+                            handleAddToCart(item);
+                          }}
+                          name="add to cart"
+                        />
+                      </li>
+                    </div>
+                  );
+                })}
+              {cart.length > 0 && (
+                <Button
+                  onClick={() => {
+                    props.history.push("/cart");
+                  }}
+                >
+                  Checkout{" "}
+                  {cart.length === 1 ? "1 Item" : `${cart.length} Items`}
+                </Button>
+              )}
+            </div>
+          )}
+
+          {user.type === "Grocer/Restaurant" &&
+            stores &&
+            stores.map(item => {
+              if (item.user_id === Number(props.match.params.id)) {
+                if (username !== item.username) {
+                  setUserName(item.username);
+                }
+                return (
+                  <div className="grocerItems">
+                    <Accordion>
+                      <Card>
+                        <Card.Header>
+                          <Accordion.Toggle
+                            as={Button}
+                            variant="link"
+                            eventKey="0"
+                          >
+                            <Hover className="hoverTitle">{item.name}</Hover>
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                          <Card.Body>
+                            Quantity: {item.quantity} {item.unit}
+                            <hr />
+                            Expiry Date: {item.expiry_date.slice(0, 10)}
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                      <Card>
+                        <Card.Header>
+                          <Accordion.Toggle
+                            as={Button}
+                            variant="link"
+                            eventKey="1"
+                          >
+                            <Hover className="hoverTitle">
+                              Image of {item.name}
+                            </Hover>
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                          <Card.Body>
+                            {item.imgurl ? (
+                              <img
+                                src={item.imgurl}
+                                style={{ height: "5rem" }}
+                              />
+                            ) : (
+                              <p>No image for this item</p>
+                            )}
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                      <br />
+                    </Accordion>
+                  </div>
+                );
               }
-              return (
-                <div className="grocerItems">
-
-                  <Accordion>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                          <Hover className="hoverTitle">{item.name}</Hover>
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="0">
-                        <Card.Body>
-                          Quantity: {item.quantity} {item.unit}
-                          <hr/>
-                          Expiry Date: {item.expiry_date.slice(0,10)}
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                          <Hover className="hoverTitle">Image of {item.name}</Hover>
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="1">
-                        <Card.Body>
-                          {item.imgurl? <img src={item.imgurl} style={{height: '5rem'}}/> : <p>No image for this item</p>}
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <br/>
-                  </Accordion>
-
-                </div>
-              );
-            }
-          })}
-      </ul>
-      <footer className="footPf">
+            })}
+        </ul>
+        <footer className="footPf">
           <span>Give a little. Help a lot.</span>
-      </footer>
+        </footer>
       </FadeInUpDiv>
     </div>
   );
