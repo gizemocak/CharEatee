9458cc237a85a74592c6db39bc7d9c622f8abe7b
Chenchen Kuang <kcchaha@gmail.com>
2019-07-02 22:03:51 -0400

    fix image display on profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -127,7 +127,7 @@ export default function Profile(props) {
                         <br/>
                         Expires: {item.expiry} 
                         <br/>
-                        <img src={item.imgurl}/>
+                        <img src={item.imgurl} style={{height: '5rem', marginTop: '1rem'}}/>
                         <Button id={index} onClick={() => {handleAddToCart(item); toggle();}}>{toggleState}</Button>
                       </ListGroup.Item>
                     </ListGroup>
@@ -148,7 +148,6 @@ export default function Profile(props) {
           )}
 
         {user.type === "Grocer/Restaurant" &&
-          stores &&
           stores.map(store => {
             if (store.id === Number(props.match.params.id)) {
               if (username !== store.username) {
