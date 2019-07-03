0cddee5c05c60be865eec1ad61211ae746a9f6a9
gizemocak <gizem_ocak@outlook.com>
2019-07-03 08:21:50 -0400

    user is able to see their order history

--- client/src/components/OrderHistory.jsx
+++ client/src/components/OrderHistory.jsx
@@ -1,39 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./NavBar";
 import { useStoreState, useStoreActions } from "easy-peasy";
 
 export default function OrderHistory(props) {
   const order = useStoreState(state => state.order);
-  let user = JSON.parse(localStorage.getItem("user"))
+  let user = JSON.parse(localStorage.getItem("user"));
+  const [products, setProducts] = useState([{}]);
+
   useEffect(() => {
-   fetch(`http://localhost:8080/api/orders/?userId=${user.user_id}&type=${user.type}`,{
-        method: 'get',
+    fetch(
+      `http://localhost:8080/api/orders/?userId=${user.user_id}&type=${
+        user.type
+      }`,
+      {
+        method: "get",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         }
-      })
+      }
+    )
       .then(res => res.json())
-      .then(res => console.log("response!!!", res))
+      .then(res => setProducts(res))
+      .then(res => console.log("state", products));
   }, []);
 
   return (
     <>
       <Navbar />
-      {order.length > 0 &&
-        JSON.parse(localStorage.getItem("user")).type === "Charity" && (
-          <div>
-            <div>Order History</div>
-            <ul>
-              {order.map(item => {
-                return <li>{item.name}</li>;
-              })}
-            </ul>
-          </div>
-        )}
-      {order.length > 0 &&
-        JSON.parse(localStorage.getItem("user")).type !== "Charity" && (
-          <div>My Donations History</div>
-        )}
+      <h3>{JSON.parse(localStorage.getItem("user")).name}</h3>
+      {JSON.parse(localStorage.getItem("user")).type === "Charity" ? (
+        <div>
+          <div>Your Order History</div>
+        </div>
+      ) : (
+        <div>Your Donations History</div>
+      )}
+      <ul>
+        {products &&
+          products.orders &&
+          products.orders.map(item => {
+            return (
+              <div>
+                {item.line_items.lineItems &&
+                  item.line_items.lineItems.map(product => {
+                    // console.log("product", product.product);
+                    return (
+                      <div>
+                        <li>{product.product}</li>
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+            // console.log("item", item.line_items.lineItems);
+          })}
+      </ul>
     </>
   );
 }
