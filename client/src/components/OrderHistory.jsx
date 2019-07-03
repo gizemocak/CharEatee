754ed835c1b30628d180c3aa629343ff89a96e4f
gizemocak <gizem_ocak@outlook.com>
2019-07-03 08:29:29 -0400

    add some styling to the order history

--- client/src/components/OrderHistory.jsx
+++ client/src/components/OrderHistory.jsx
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./NavBar";
-import { useStoreState, useStoreActions } from "easy-peasy";
+import "../style/Profile.scss";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import posed from "react-pose";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import styled, { keyframes } from "styled-components";
+import { fadeInUp } from "react-animations";
+const FadeInUpAnimation = keyframes`${fadeInUp}`;
+const FadeInUpDiv = styled.div`
+  animation: 500ms ${FadeInUpAnimation};
+`;
 
 export default function OrderHistory(props) {
-  const order = useStoreState(state => state.order);
   let user = JSON.parse(localStorage.getItem("user"));
   const [products, setProducts] = useState([{}]);
+
+  const Hover = posed.div({
+    hoverable: true,
+    pressable: true,
+    init: {
+      scale: 1,
+      boxShadow: "0px 0px 0px rgba(0,0,0,0)"
+    },
+    hover: {
+      scale: 1.2,
+      boxShadow: "0px 5px 10px rgba(0,0,0,0.2)"
+    },
+    press: {
+      scale: 1.1,
+      boxShadow: "0px 2px 5px rgba(0,0,0,0.1)"
+    }
+  });
 
   useEffect(() => {
     fetch(
@@ -27,33 +57,42 @@ export default function OrderHistory(props) {
   return (
     <>
       <Navbar />
-      <h3>{JSON.parse(localStorage.getItem("user")).name}</h3>
-      {JSON.parse(localStorage.getItem("user")).type === "Charity" ? (
-        <div>
-          <div>Your Order History</div>
-        </div>
-      ) : (
-        <div>Your Donations History</div>
-      )}
+      <div className="greeting">
+        <h3>{JSON.parse(localStorage.getItem("user")).name}</h3>
+        {JSON.parse(localStorage.getItem("user")).type === "Charity" ? (
+          <div>
+            <div>Your Order History</div>
+          </div>
+        ) : (
+          <div>Your Donations History</div>
+        )}
+      </div>
       <ul>
-        {products &&
-          products.orders &&
-          products.orders.map(item => {
-            return (
-              <div>
-                {item.line_items.lineItems &&
-                  item.line_items.lineItems.map(product => {
-                    // console.log("product", product.product);
-                    return (
-                      <div>
-                        <li>{product.product}</li>
-                      </div>
-                    );
-                  })}
-              </div>
-            );
-            // console.log("item", item.line_items.lineItems);
-          })}
+        <div className="itemList">
+          {products &&
+            products.orders &&
+            products.orders.map(item => {
+              return (
+                <div>
+                  {item.line_items.lineItems &&
+                    item.line_items.lineItems.map(product => {
+                      console.log("product", product.product);
+                      return (
+                        <div className="singleItem">
+                          <ListGroup>
+                            <ListGroup.Item action variant="warning">
+                              {product.product}
+                            </ListGroup.Item>
+                          </ListGroup>
+                          <br />
+                        </div>
+                      );
+                    })}
+                </div>
+              );
+              // console.log("item", item.line_items.lineItems);
+            })}
+        </div>
       </ul>
     </>
   );
